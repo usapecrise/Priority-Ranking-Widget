d61f323c10466dbec3765f492fedf31fb738c45b
usapecrise <usapecrise@gmail.com>
2026-06-12 12:24:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document.getElementById("rankingContainer").innerHTML = `
-    <div style="background:red;color:white;padding:20px;">
-        JAVASCRIPT IS WORKING
+const container = document.getElementById("rankingContainer");
+
+container.innerHTML = `
+    <div class="ranking-item">
+        <div class="topic-title">
+            Test Topic
+        </div>
+
+        <div class="rank-row">
+            <span class="rank-label">Rank:</span>
+
+            <select class="rank-select">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+            </select>
+        </div>
     </div>
 `;
